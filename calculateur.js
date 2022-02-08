e1b57aae80316f26489468734217d716c86fd705
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2022-02-08 07:02:47 -0500

    first commit

---
+++ calculateur.js
@@ -0,0 +1,38 @@
+// semelle
+const longueurBatisse = document.querySelector(".longueur_batisse");
+const selectLongueurBatisse = document.querySelector(
+  "#select_longueur_batisse"
+);
+const largeurBatisse = document.querySelector(".largeur_batisse");
+const selectLargeurBatisse = document.querySelector(".select_largeur_batisse");
+
+const largeurSemelle = document.querySelector(".largeur_semelle");
+const selectLongueurBatisse = document.querySelector("#select_largeur_semelle");
+const epaisseurSemelle = document.querySelector(".epaisseur_semelle");
+const selectEpaisseurSemelle = document.querySelector(
+  ".select_epaisseur_semelle"
+);
+
+// murs
+const longueurBatisseMurs = document.querySelector(".longueur_batisse_murs");
+const selectLongueurBatisseMurs = document.querySelector(
+  "#select_longueur_batisse_murs"
+);
+const largeurBatisseMurs = document.querySelector(".largeur_batisse_murs");
+const selectLargeurBatisseMurs = document.querySelector(
+  ".select_largeur_batisse_murs"
+);
+
+const hauteurMurs = document.querySelector(".hauteur_murs");
+const selectHauteurMurs = document.querySelector("#select_hauteur_murs");
+const epaisseur_murs = document.querySelector(".eppaisseur_murs");
+const selectEpaisseurMurs = document.querySelector(".select_epaisseur_murs");
+
+// totaux
+const totalSemelle = document.querySelector(".total_semelle");
+const totalMurs = document.querySelector(".total_murs");
+const totalPlancher = document.querySelector(".total_plancher");
+const totalSonotube = document.querySelector(".total_sonotube");
+
+const clear = document.querySelector(".clear_all");
+const btnCalculate = document.querySelector(".calculate");
