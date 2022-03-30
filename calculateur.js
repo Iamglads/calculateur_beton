71ebeeaf887b21f845d781f9729e011def79c053
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2022-03-30 11:12:18 -0400

    Gérer le click dans le js

--- calculateur.js
+++ calculateur.js
@@ -1,51 +1,60 @@
+const form = document.querySelector("#form");
+const inputs = document.querySelectorAll("input");
+const btnCalculate = document.querySelector(".btn-calculate");
+const btnClear = document.querySelector(".btn-clear");
+
+function multiply(paramOne, paramTwo) {
+  return paramOne * paramTwo;
+}
+
 function calcule(form) {
   // semelle
 
-  let lg1 = eval(form.Length1.value);
+  var longueurSemelle = form.Length1.value;
 
-  if (lg1 > 0) {
-    lg1 = lg1 * eval(form.L1U.value);
-    let l1 = eval(form.Width1.value) * eval(form.La1U.value);
-    l1 = (l1 + lg1) * 2;
-    let h1 = eval(form.Width4.value) * eval(form.La4U.value);
-    let p1 = eval(form.Epaisseur1.value) * eval(form.E1U.value);
+  if (longueurSemelle > 0) {
+    longueurSemelle = multiply(longueurSemelle, form.L1U.value);
+    var l1 = multiply(form.largeurSemelle.value, form.La1U.value);
+    l1 = (l1 + longueurSemelle) * 2;
+    var h1 = multiply(form.Width4.value, form.La4U.value);
+    var p1 = multiply(form.Epaisseur1.value, form.E1U.value);
   } else {
-    let h1 = eval(form.Width1.value) * eval(form.La1U.value);
-    let l1 = eval(form.Width4.value) * eval(form.La4U.value);
-    let p1 = eval(form.Epaisseur1.value) * eval(form.E1U.value);
+    var h1 = multiply(form.largeurSemelle.value, form.La1U.value);
+    var l1 = multiply(form.Width4.value, form.La4U.value);
+    var p1 = multiply(form.Epaisseur1.value, form.E1U.value);
   }
 
   // Murs
-  let lg2 = eval(form.Length2.value);
+  var longueurMurs = form.Length2.value;
 
-  if (lg2 > 0) {
-    lg2 = lg2 * eval(form.L2U.value);
-    let l2 = eval(form.Width2.value) * eval(form.La2U.value);
-    l2 = (l2 + lg2) * 2;
-    let h2 = eval(form.Hauteur2.value) * eval(form.H2U.value);
-    let p2 = eval(form.Epaisseur2.value) * eval(form.E2U.value);
+  if (longueurMurs > 0) {
+    longueurMurs = longueurMurs * form.L2U.value;
+    var l2 = multiply(form.Width2.value, form.La2U.value);
+    l2 = (l2 + longueurMurs) * 2;
+    var h2 = multiply(form.Hauteur2.value, form.H2U.value);
+    var p2 = multiply(form.Epaisseur2.value, form.E2U.value);
   } else {
-    let h2 = eval(form.Width2.value) * eval(form.La2U.value);
-    let l2 = eval(form.Hauteur2.value) * eval(form.H2U.value);
-    let p2 = eval(form.Epaisseur2.value) * eval(form.E2U.value);
+    var h2 = multiply(form.Width2.value, form.La2U.value);
+    var l2 = multiply(form.Hauteur2.value, form.H2U.value);
+    var p2 = multiply(form.Epaisseur2.value, form.E2U.value);
   }
 
   // Plancher
-  let l3 = eval(form.Length3.value) * eval(form.L3U.value);
-  let h3 = eval(form.Width3.value) * eval(form.La3U.value);
-  let p3 = eval(form.Epaisseur3.value) * eval(form.E3U.value);
+  var l3 = multiply(form.Length3.value, form.L3U.value);
+  var h3 = multiply(form.Width3.value, form.La3U.value);
+  var p3 = multiply(form.Epaisseur3.value, form.E3U.value);
 
   // Sonotube
-  let rayon = eval(form.rayonSonotube.value) * eval(form.R4U.value);
-  let l4 = eval(form.Longueur4.value) * eval(form.L4U.value);
-  let q4 = eval(form.Quantite4.value);
+  var rayon = multiply(form.rayonSonotube.value, form.R4U.value);
+  var l4 = multiply(form.Longueur4.value, form.L4U.value);
+  var q4 = form.Quantite4.value;
 
-  let totalSemelle = Math.round(l1 * h1 * p1 * 100 + 0.499) / 100;
-  let totalMurs = Math.round(l2 * h2 * p2 * 100 + 0.499) / 100;
-  let totalPlancher = Math.round(l3 * h3 * p3 * 100 + 0.499) / 100;
-  let totalSonotube =
-    (Math.round(PI * Math.pow(rayon, 2) * l4 * q4) * 100) / 100;
-  let total = totalSemelle + totalMurs + totalPlancher + totalSonotube;
+  var totalSemelle = Math.round(l1 * h1 * p1 * 100 + 0.499) / 100;
+  var totalMurs = Math.round(l2 * h2 * p2 * 100 + 0.499) / 100;
+  var totalPlancher = Math.round(l3 * h3 * p3 * 100 + 0.499) / 100;
+  var totalSonotube =
+    (Math.round(Math.PI * Math.pow(rayon, 2) * l4 * q4) * 100) / 100;
+  var total = totalSemelle + totalMurs + totalPlancher + totalSonotube;
 
   form.resultTotalSemelle.value = totalSemelle;
   form.resultTotalMurs.value = totalMurs;
@@ -55,3 +64,13 @@ function calcule(form) {
   // display total
   form.resultTotalAll.value = Math.round(total * 100) / 100;
 }
+
+btnCalculate.addEventListener("click", () => {
+  calcule(form);
+});
+
+btnClear.addEventListener("click", () => {
+  inputs.forEach((input) => {
+    input.value = "";
+  });
+});
